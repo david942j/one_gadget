509d3edda00435da2e9c67e31c2ffa1b6ca4317d
david942j <david942j@gmail.com>
2017-03-21 23:56:13 +0800

    add test of bin/one_gadget

---
+++ spec/bin_spec.rb
@@ -0,0 +1,17 @@
+describe 'Binary' do
+  before(:all) do
+    @bin = File.join(__dir__, '..', 'bin', 'one_gadget')
+    @lib = File.join(__dir__, '..', 'lib')
+  end
+
+  it 'help' do
+    expect(`env ruby -I#{@lib} #{@bin}`).to eq <<-EOS
+Usage: one_gadget [file] [options]
+    -b, --build-id BuildID           BuildID[sha1] of libc.
+    -f, --[no-]force-file            Force search gadgets in file instead of build id first.
+    -r, --[no-]raw                   Output gadgets offset only, split with one space.
+    -s, --script exploit-script      Run exploit script with all possible gadgets.
+                                     The script will be run as 'exploit-script $offset'.
+    EOS
+  end
+end
