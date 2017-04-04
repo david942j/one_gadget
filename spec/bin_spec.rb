075ca491c62fecdb915950c6c31afff45566d52c
david942j <david942j@gmail.com>
2017-04-04 15:29:11 +0800

    add gem version option

--- spec/bin_spec.rb
+++ spec/bin_spec.rb
@@ -12,6 +12,7 @@ Usage: one_gadget [file] [options]
     -r, --[no-]raw                   Output gadgets offset only, split with one space.
     -s, --script exploit-script      Run exploit script with all possible gadgets.
                                      The script will be run as 'exploit-script $offset'.
+        --version                    Current gem version.
     EOS
   end
 end
