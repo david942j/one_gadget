38abecc6edeb8db1fbce2f95a5e41914b8db6cea
david942j <david942j@gmail.com>
2020-06-03 00:00:24 +0800

    Add 2.29, 2.30, and 2.31 builds (#118)

---
+++ lib/one_gadget/builds/libc-2.30-135c0b4a030ec9b6306ed6b621e1e0dedfd614a3.rb
@@ -0,0 +1,23 @@
+require 'one_gadget/gadget'
+# https://gitlab.com/david942j/libcdb/blob/master/libc/libc6-i386-2.30-7/lib32/libc-2.30.so
+# 
+# Intel 80386
+# 
+# GNU C Library (Debian GLIBC 2.30-7) stable release version 2.30.
+# Copyright (C) 2019 Free Software Foundation, Inc.
+# This is free software; see the source for copying conditions.
+# There is NO warranty; not even for MERCHANTABILITY or FITNESS FOR A
+# PARTICULAR PURPOSE.
+# Compiled by GNU CC version 9.3.0.
+# libc ABIs: UNIQUE IFUNC ABSOLUTE
+# For bug reporting instructions, please see:
+# <http://www.debian.org/Bugs/>.
+
+build_id = File.basename(__FILE__, '.rb').split('-').last
+OneGadget::Gadget.add(build_id, 1307451,
+                      constraints: ["ebp is the GOT address of libc", "eax == NULL"],
+                      effect: "execl(\"/bin/sh\", eax)")
+OneGadget::Gadget.add(build_id, 1307452,
+                      constraints: ["ebp is the GOT address of libc", "[esp] == NULL"],
+                      effect: "execl(\"/bin/sh\", [esp])")
+
