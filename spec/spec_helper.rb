605c917967d914d04a7f945eecf49f7c73139932
david942j <david942j@gmail.com>
2025-10-31 10:09:49 +0800

    Replace codeclimage with Qlty (#273)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 # frozen_string_literal: true
 
 require 'simplecov'
+require 'simplecov_json_formatter'
 
 SimpleCov.start do
+  formatter SimpleCov::Formatter::MultiFormatter.new([
+                                                       SimpleCov::Formatter::JSONFormatter,
+                                                       SimpleCov::Formatter::HTMLFormatter
+                                                     ])
   add_filter '/spec/'
   add_filter '/lib/one_gadget/builds/'
 end
